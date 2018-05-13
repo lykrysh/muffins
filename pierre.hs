3447639354a01f1f9cf607ae021fd44f0397bb7e
lykrysh <lykrysh@email.com>
2018-05-12 19:36:36 -0700

    Do series of monad

--- pierre.hs
+++ pierre.hs
@@ -13,6 +13,26 @@ landRight n (left, right)
   | abs (left - (right + n)) < 4 = Just (left, right + n)
   | otherwise = Nothing
 
+routine1 :: Maybe Pole
+routine1 = case landLeft 1 (0,0) of
+            Nothing -> Nothing
+            Just pole1 -> case landRight 4 pole1 of
+                            Nothing -> Nothing
+                            Just pole2 -> case landLeft 2 pole2 of
+                                            Nothing -> Nothing
+                                            Just pole3 -> landLeft 1 pole3
+
+routine2 :: Maybe Pole
+routine2 = do
+  pole0 <- return (0,0)
+  pole1 <- landLeft 1 pole0
+  pole2 <- landRight 4 pole1
+  pole3 <- landLeft 2 pole2
+  landLeft 1 pole3
+
 main = do
   putStrLn $ show $ return (0,0) >>= landRight 2 >>= landLeft 2 >>= landRight 2
   putStrLn $ show $ return (0,0) >>= landLeft 1 >>= landRight 4 >>= landLeft (-1) >>= landRight (-2)
+  putStrLn $ show $ return (0,0) >>= landLeft 1 >> Nothing >>= landRight 1
+  putStrLn $ show routine1
+  putStrLn $ show routine2
